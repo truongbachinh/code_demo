9cff8b3eda701159ec2e8fda1337b78f250f7d9c
David Cody <truongbachinh@gmail.com>
2021-10-25 17:33:53 +0700

    Ad' '

--- Study/src/main/java/study/controller/HomeController.java
+++ Study/src/main/java/study/controller/HomeController.java
@@ -1,11 +1,22 @@
 package study.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import study.model.Company;
 
 @Controller
+
 public class HomeController {
+
+    @Autowired
+    @Qualifier("company")
+    Company company;
 
     @RequestMapping("/home")
     public String getHome()
@@ -16,5 +27,12 @@ public class HomeController {
     public String getIndex()
     {
         return "index";
+    }
+
+
+    @RequestMapping(value = "/greenwich", method = RequestMethod.GET)
+    public String getGreenwich(ModelMap modelMap){
+        modelMap.addAttribute("company", company);
+        return "greenwich";
     }
 }
---
+++ Study/src/main/java/study/model/Company.java
@@ -0,0 +1,25 @@
+package study.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Company {
+    private String name;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
